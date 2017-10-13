16ee18d4a32732f0d4a578ed3105f28622ba4670
LuybaShevchenko <Luyba.S@yandex,ru>
2017-10-13 16:21:08 +0700

    Вывод данных

---
+++ Index5.php
@@ -0,0 +1,30 @@
+<?php
+$groups=[
+'101'=>'4.2',
+'102'=>'4.0',
+'103'=>'4.8',
+'104'=>'4.6',
+'105'=>'3.8',
+];
+$sum =0;
+$avg=0;
+$array=[];
+foreach ($groups as  $group){
+	$sum+=$group;
+}
+	
+$avg= $sum/count($groups);
+
+
+foreach ($groups as $index => $group)
+if ($avg < $group){
+
+	//$gx=$index;
+$array[]=	$index;
+
+}
+
+echo implode ('-', $array) ;
+
+?>
+
